48a3b262b7b3057496463477cf604e662fd835f9
Kelvin <vokemalosh32@gmail.com>
2022-07-18 18:10:31 +0300

    commit tree 12adac97ed9baaa5c9dd4b264c5d31b77057026f

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,7 +6,7 @@ function Item({ name, category }) {
   function handleAddToCartClick() {
     setIsInCart((isInCart) => !isInCart);
   }
-
+ 
   return (
     <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
